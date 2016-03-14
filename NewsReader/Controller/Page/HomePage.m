e57eeb0674e88e9ee400b752db3c0c412844c71d
ChrisYu <nihaoyff@163.com>
2016-03-15 00:08:33 +0800

    整理相关目录

---
+++ NewsReader/Controller/Page/HomePage.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NewsReader
+//
+//  Created by 禹飞飞 on 16/3/14.
+//  Copyright © 2016年 chris. All rights reserved.
+//
+
+#import "HomePage.h"
+
+@interface HomePage ()
+
+@end
+
+@implementation HomePage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+@end
