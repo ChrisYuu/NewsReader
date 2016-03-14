163a8e8ce38761399081b2a5a645ab2751b8fb4b
ChrisYu <nihaoyff@163.com>
2016-03-15 00:44:23 +0800

    修改app图标，修改启动图

--- NewsReader/Controller/Page/HomePage.m
+++ NewsReader/Controller/Page/HomePage.m
@@ -16,12 +16,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self testDeviceSize];
 
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     
+}
+
+- (void)testDeviceSize {
+    CGSize size = [UIScreen mainScreen].bounds.size;
+    if(IsiPhone6P) {
+        FxLog(@"6Plus- %f, %f", size.width, size.height);
+    }else if(IsiPhone6) {
+        FxLog(@"6- %f, %f", size.width, size.height);
+    }else if(IsiPhone5) {
+        FxLog(@"5- %f, %f", size.width, size.height);
+    }else if(IsiPhone4) {
+        FxLog(@"4 or less- %f, %f", size.width, size.height);
+    }
 }
 
 @end
